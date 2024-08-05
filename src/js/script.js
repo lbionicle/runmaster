d1fe16b7b6a688afb9b28d22e848869dbbe13991
NikolayTS <kolya.tsymbal45@gmail.com>
2024-08-05 17:47:19 +0300

    add modals

--- src/js/script.js
+++ src/js/script.js
@@ -42,3 +42,32 @@ const onChangeContent = (item) => {
 
 onChangeContent(".catalog-item__content")
 onChangeContent(".catalog-item__newcontent")
+
+const closeModal = (itemClass) => {
+    document.querySelectorAll(itemClass).forEach(item => item.addEventListener("click", () => {
+        document.querySelector(".overlay").style.display = "none";
+        document.querySelectorAll(".modal").forEach(item => item.style.display = "none");
+    }))
+}
+
+const openModal = (attr) => {
+    document.querySelectorAll(`[data-modal="${attr}"]`).forEach(item => {
+
+        item.addEventListener("click", (e) => {
+            const modal = document.querySelector(`#${attr}`)
+
+            document.querySelector(".overlay").style.display = "block"
+            modal.style.display = "block"
+
+            if (attr === "order") {
+                modal.querySelector(".modal__descr").textContent = e.target.parentElement.parentElement.querySelector(".catalog-item__subtitle").textContent
+            }
+        })
+    })
+}
+
+closeModal(".modal__close")
+closeModal(".overlay")
+
+openModal("consultation")
+openModal("order")
